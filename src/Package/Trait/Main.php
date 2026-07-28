77191aab0eef1e2e5068ddec7b6199cba6baa628
Remco van der Velde <remco@universeorange.com>
2026-07-28 22:25:29 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -216,7 +216,7 @@ trait Main {
         if($connection === null){
             throw new Exception('Connection not found aborting...');
         }
-        breakpoint($object->config('route.current');
+        breakpoint($object->config('route.current'));
         /*
         $route = Route::controller($route);
         $destination->set('controller',  $route->controller);
