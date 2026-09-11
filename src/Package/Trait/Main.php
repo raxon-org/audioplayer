3a7d9f00be86ddd1a7e5b97c0e6ab38fb397c04a
Remco van der Velde <remco@universeorange.com>
2026-09-11 19:38:00 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -206,8 +206,8 @@ trait Main {
             throw new Exception('System.Server.Extension.json not found aborting...');
         }
         $list_search = [];
-        foreach($read->data() as $extension){
-            dd($extension);           
+        foreach($read->data('System.Server') as $extension){
+            dd($extension);
             $list_search[$extension->name] = $extension->uuid;
         }
         /*
